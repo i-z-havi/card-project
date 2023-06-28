b245d3afb7fb8728e43af31e38db2ae100b9f4ac
i-z-havi <136263166+i-z-havi@users.noreply.github.com>
2023-06-28 22:45:10 +0300

    MAP IS FINISHED
    JUST HAD TO CHANGE AXIOS HEADER AND IT WOOOOOORKED

--- src/cards/pages/CardDetailsPage.jsx
+++ src/cards/pages/CardDetailsPage.jsx
@@ -6,31 +6,35 @@ import PageHeader from "../../components/PageHeader";
 import { useLoadScript } from "@react-google-maps/api";
 import Map from "../../components/Map";
 import { useState } from "react";
+import {key} from "../services/mapApiService.js";
 
 export default function CardDetailsPage() {
-  const key= "AIzaSyBYWQ_bT1b39uMS_iIMCmunhYHtvMiFiac"
-  const { id } = useParams();
   const {value,handleGetCard} = useCards();
+  const { id } = useParams();
   useEffect(()=>{
     handleGetCard(id)
+
   },[])
-  console.log(value.card);
   const {isLoaded}= useLoadScript({
-    googleMapsApiKey: "AIzaSyBYWQ_bT1b39uMS_iIMCmunhYHtvMiFiac",
+    googleMapsApiKey: key,
   });
 
+
+  if(!value.card) return<div>loading...</div>
   return (   
     <Container sx={{
       paddingTop: 8,
       justifyContent: "center", 
       alignItems: "center",
     }}>
-    {value.card&&
+  {(value.card&&isLoaded)?
+  <div>
    <PageHeader title="Card Details"
-          subtitle={"This page contains all data for the card "+value.card.address.state}
+          subtitle={"This page contains all data for the card "+value.card.address}
           />
-    }
-    {isLoaded&&<Map key={key} address={value.card}/>}
+    <Map key={key} address={value.card.address}/>
+    </div>:null
+  }
     </Container>
   )
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import { GoogleMap,Marker } from "@react-google-maps/api";
+import{key, geocodeJson,getPosition,geocodingQuery} from "../cards/services/mapApiService.js"
+import { useEffect } from 'react';
+import axios from 'axios';
+import useAxios from '../hooks/useAxios.js';
+import { useState } from 'react';
+import { useUser } from '../users/providers/UserProvider.jsx';
 
-export default function Map(key,address) {
-    const geocodeJson = 'https://maps.googleapis.com/maps/api/geocode/json';
-    // const url = `${geocodeJson}?key=${key}&latlng=${lat},${lng}`;
-    const position=`${geocodeJson}?address=&key=${key}`
-    console.log(address);
-    
+export default function Map(address) {
+  // results['0'].geometry.location.lat
+  // results['0'].geometry.location.lng
+  const {token}=useUser();
+  useEffect(()=>{
+    getDataFromAPI();
+  },[])
+
+  const [data,setData]=useState();
+
+  const getDataFromAPI = async () => {
+    try {
+      delete axios.defaults.headers.common["x-auth-token"];
+      const { data } = await axios.get(getPosition(parseAddress(address).replace(' ',"%20")));
+      axios.defaults.headers.common["x-auth-token"] = token;
+      console.log(data.results);
+      setData(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+    const parseAddress=(address)=>{
+      let parsedAddress="";
+      parsedAddress+=address.address.houseNumber+"+"+address.address.street+"+"+address.address.city;
+      return parsedAddress;
+    }
   return (
     <Box>
-        <GoogleMap zoom={10} center={{lat:40, lng:30}} mapContainerStyle={{width:500,height:500}}>
-            <Marker position={{lat:40,lng:30}} />
+        {data?
+        <GoogleMap zoom={20} center={{lat:data.results['0'].geometry.location.lat, lng:data.results['0'].geometry.location.lng}} mapContainerStyle={{width:500,height:500}}>
+            <Marker position={{lat:data.results['0'].geometry.location.lat,lng:data.results['0'].geometry.location.lng}} />
         </GoogleMap>
+        :null}
     </Box>
   )
 }
