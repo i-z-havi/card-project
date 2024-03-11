bd62b9196983f508f992de2e8ab69872aa699bd4
i-z-havi <136263166+i-z-havi@users.noreply.github.com>
2024-03-11 21:06:45 +0200

    Fixed isBusiness and isAdmin

--- src/users/services/localStorageService.js
+++ src/users/services/localStorageService.js
@@ -1,16 +1,22 @@
 import jwtDecode from "jwt-decode";
 
-const TOKEN="token";
-export const setTokenInLocalStorage=(encryptedToken)=> localStorage.setItem(TOKEN,encryptedToken)
+const TOKEN = "token";
+export const setTokenInLocalStorage = (encryptedToken) =>
+  localStorage.setItem(TOKEN, encryptedToken);
 
-export const removeToken=()=> localStorage.removeItem(TOKEN)
+export const removeToken = () => localStorage.removeItem(TOKEN);
 
-export const getToken=()=> localStorage.getItem(TOKEN)
+export const getToken = () => localStorage.getItem(TOKEN);
 
-export const getUser=()=>{
-    try{const myToken = getToken();
-    return jwtDecode(myToken);
-    } catch(error){
-        return null;
-    }
-}
+export const getUser = () => {
+  try {
+    const myToken = localStorage.getItem(TOKEN);
+    const userDetails = jwtDecode(myToken);
+    userDetails.isBusiness = userDetails.isBusiness === "true";
+    userDetails.isAdmin = userDetails.isAdmin === "true";
+
+    return userDetails;
+  } catch (error) {
+    return null;
+  }
+};
